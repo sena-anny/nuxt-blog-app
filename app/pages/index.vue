05f6362c247f5362791abcfe80ef3fce3b86e4a8
KJ <cozykoji18@gmail.com>
2019-10-14 19:37:33 +0900

    nuxt blog app first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="container">
+    <el-card style="flex: 1;">
+      <div slot="header" class="clearfix">
+        <span>ログイン</span>
+      </div>
+      <form>
+        <div class="form-content">
+          <span>ユーザーID</span>
+          <el-input placeholder="" v-model="formData.id" />
+        </div>
+        <div class="form-content">
+          <el-checkbox v-model="isCreateModel">アカウントを作成する</el-checkbox>
+        </div>
+        <div class="text-right">
+          <el-button type="primary">{{buttonText}}</el-button>
+        </div>
+      </form>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      isCreateModel: false,
+      formData: {
+        id: ''
+      }
+    }
+  },
+  computed: {
+    buttonText() {
+      return this.isCreateModel ? '新規登録' : 'ログイン'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-content {
+  margin: 16px 0;
+}
+</style>
---
+++ app/pages/posts/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container posts-page">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>新着投稿</span>
+      </div>
+      <el-table
+        :data="showPosts"
+        style="width: 100%"
+        class="table"
+      >
+        <el-table-column
+            prop="title"
+            label="タイトル">
+        </el-table-column>
+        <el-table-column
+            prop="user.id"
+            label="投稿者"
+            width="180" >
+        </el-table-column>
+        <el-table-column
+            prop="created_at"
+            label="投稿日時"
+            width="240" >
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+    computed: {
+        showPosts() {
+            return [
+                {
+                    id: '001',
+                    title: 'How to develop Nuxt.js Application',
+                    body: 'Lorem aaaaaaaa...',
+                    created_at: '2019/10/13 20:00:00',
+                    user: {
+                        id: 'kj'
+                    }
+                },
+                {
+                    id: '002',
+                    title: 'firebase',
+                    body: 'Lorem aaaaaaaa...',
+                    created_at: '2019/10/13 21:00:00',
+                    user: {
+                        id: 'kj'
+                    }
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.posts-page .el-table__row {
+    cursor: pointer;
+}
+</style>
